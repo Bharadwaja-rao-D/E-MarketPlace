97752a46c2139df726eeb291f4f20c27d7bcba6d
Bharadwaja-rao-D <cs20btech11012@iith.ac.in>
2023-03-28 14:57:50 +0530

    put and delete for product api added

--- emarket/api/models/productModels.py
+++ emarket/api/models/productModels.py
@@ -1,4 +1,6 @@
+import os
 from django.db import models
+from django.dispatch import receiver
 from api.models.customerModels import Customer
 
 
@@ -45,3 +47,14 @@ class Interested(models.Model):
 
     class Meta:
         unique_together = (('product', 'buyer'),)
+
+@receiver(models.signals.post_delete, sender=Image)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Product` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
