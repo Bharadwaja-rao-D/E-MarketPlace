e7d746aa5383787efa135bc17fb0f7468231ff6c
Manojbhargav1305 <cs20btech11022@iith.ac.in>
2023-04-28 11:11:41 +0530

    added pagination to myproducts and sold products

--- emarket/client/src/pages/SoldProducts.js
+++ emarket/client/src/pages/SoldProducts.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ProductList from "../components/productComponents/ProductList";
 import DisplayData from "../components/commonComponents/DisplayData";
+import Pagenation from "../components/commonComponents/Pagenation";
+import { useEffect, useState } from "react";
 
 function SoldProducts() {
-  const url = "products/sold/";
+    const [page, setPage] = useState(1);
+  const [url, setURL] = useState("products/sold/?page="+page);
+
+    useEffect(() => {
+        setURL("products/sold/?page="+page)
+    }, [page]);
   return (
     <>
       <h2 className="page-name">Sold Products</h2>
       <div className="products-display">
         <DisplayData url={url} Child={ProductList} nav_to={null} />
+        <Pagenation setPage={setPage} />
       </div>
     </>
   );
